3536cc10ad5e3bfb77a096ec919c59e799473036
shirocub <shirocub@gmail.com>
2014-06-18 22:12:40 +0900

    年度を取得するプログラムを作る

---
+++ FiscalYear/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiscalYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(DateTime.Now.ToString("yyyyMM"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
