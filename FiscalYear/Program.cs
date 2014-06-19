e981a9e441107fcdcfcf995fef41c2babd9b42e1
shirocub <shirocub@gmail.com>
2014-06-19 21:58:39 +0900

    今年度を出すように変更しました。

--- FiscalYear/Program.cs
+++ FiscalYear/Program.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FiscalYear
+﻿namespace FiscalYear
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int FiscalYear = 0;
+            switch (DateTime.Now.Month)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    FiscalYear = DateTime.Now.Year - 1;
+                    break;
+                default:
+                    FiscalYear = DateTime.Now.Year;
+                    break;
+            }
 
-            Console.WriteLine(DateTime.Now.ToString("yyyyMM"));
-
+            Console.WriteLine(string.Format("今年度は{0}年度です。", FiscalYear));            
             Console.ReadKey();
 
         }
